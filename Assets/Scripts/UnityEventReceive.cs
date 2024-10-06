1f422eab3584667bdb1feb3bddab89a9e89b47ca
Goat King <joaki2003@gmail.com>
2024-10-06 12:19:15 +0200

    More test stuff

---
+++ Assets/Scripts/UnityEventReceive.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class UnityEventReceive : MonoBehaviour
+{
+    #region Signal Quality Pipeline Events
+    public void OnSignalQualityAvailable(List<ChannelStates> channelStates)
+    {
+        string channelStatesString = string.Empty;
+        for (int i = 0; i < channelStates.Count; i++)
+            channelStatesString += string.Format("{0}: {1}, ", i + 1, channelStates[i].ToString());
+        Debug.Log(channelStatesString);
+    }
+    #endregion
+}*/
---
+++ Assets/feedback.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feedback : MonoBehaviour
+{
+    public void flashTag1()
+    {
+        Debug.Log("Tag 1 selected!");
+    }
+    public void flashTag2()
+    {
+        Debug.Log("Tag 2 selected!");
+    }
+    public void flashTag3()
+    {
+        Debug.Log("Tag 3 selected!");
+    }
+    public void flashTag4()
+    {
+        Debug.Log("Tag 4 selected!");
+    }
+    public void flashTag5()
+    {
+        Debug.Log("Tag 5 selected!");
+    }
+}
